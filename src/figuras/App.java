c6c1ffea7966f26248a5a6db3a248d86139cf3f4
Misscott <misscotters@gmail.com>
2024-01-24 17:45:57 +0100

    changed first sysout

--- src/figuras/App.java
+++ src/figuras/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         int option;
         do {
-            System.out.println("Calculadora de Área by Pau");
+            System.out.println("Calculadora de Área de Pau");
             System.out.println("Selecciona una opción:");
             System.out.println("1. Círculo");
             System.out.println("2. Cuadrado");
